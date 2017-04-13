38f2c07c25b674bea313c049a2919f5c871a5cfa
Bartlomiej Wilczynski <me@bwilczynski.com>
2017-04-13 23:22:45 +0200

    Initial commit

---
+++ FileBroker/Controllers/FileBrokerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+
+namespace FileBroker.Controllers
+{
+    [Route("api/files")]
+    public class FileController : Controller
+    {
+        [HttpPost]
+        public void Post([FromBody]string message)
+        {
+            var body = Encoding.UTF8.GetBytes(message);
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare("files", "fanout");
+                    channel.BasicPublish(exchange: "files", routingKey: "", basicProperties: null, body: body);
+                }
+            }
+        }
+    }
+}
